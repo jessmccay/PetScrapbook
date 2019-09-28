14b6cb86f1e948781a1b2ac834ef73bdc37c5b62
jessmccay <jessmccay@gmail.com>
2019-09-28 11:37:59 -0400

    view list of scrapbooks, use data binding to navigate to the selected scrapbook page, display data on new scrapbook page

--- app/views/scrapbook-page/scrapbook-page.js
+++ app/views/scrapbook-page/scrapbook-page.js
@@ -1,27 +1,49 @@
-var observable = require("data/observable");
+var observable = require("tns-core-modules/data/observable");
+var observableArray = require("tns-core-modules/data/observable-array");
+var frame = require("ui/frame");
+
+function scrapbookPageModel() {
+    var model = new observable.Observable();
+        // year = "1988";
+        // month = "10";
+        // day = "19";
+        model.genders = ["Female", "Male", "Other"];
+        model.calcAge = function(year, month, day) {
+            var date = new Date(year, month, day);
+            var now = Date.now();
+            var diff = Math.abs(now - date) / 1000 / 31536000;
+            
+            return diff.toFixed(1);
+        };
+    
+    return model;
+}
 
 exports.onLoaded = function(args) {
     var page = args.object;
-    var scrapbook = new observable.fromObject({
-        genders: ["Female", "Male", "Other"],
-        gender: 2,
-        title: "hello",
-        date: "1988/10/19",
-        calcAge: function(birthDate) {
-            var now = Date.now();
-            var diff = Math.abs(now - birthDate)/ 1000 / 31536000;
-            return diff.toFixed(0);
-        }
-      
-    });
+    var scrapbook;
+    page.year = "1988";
+    page.month = "10";
+    page.day = "19";
+
+    if(page.navigationContext != null) {
+        scrapbook = page.navigationContext.model;
+    }
+    else {
+        scrapbook = new observable.fromObject({
+            pages: new observableArray.ObservableArray(new scrapbookPageModel())
+        });
+    }
 
     page.bindingContext = scrapbook;
-}
+};
 
-exports.onTap = function(args) {
-    var page = args.object; 
-    var scrapbook = page.bindingContext; 
-    console.log("You have made " + scrapbook.title); 
-    console.log("Age: " + scrapbook.calcAge(scrapbook.date)); 
-    console.log("Gender selected:" + (scrapbook.genders[scrapbook.gender]));
-}   
+exports.onItemTap = function(args) {
+    var page = args.object;
+    var scrapbook = page.bindingContext;
+
+    frame.topmost().navigate({
+        moduleName: "views/scrapbookUpdate-page/scrapbookUpdate-page",
+        context: { model: scrapbook, index: args.index }
+    });
+};
