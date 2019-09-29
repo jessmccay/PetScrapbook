b7462e9ab84e7ccea1700703f3be28aefd53c248
jessmccay <jessmccay@gmail.com>
2019-09-29 00:15:39 -0400

    use file system on local phone storage to save/update/retrieve data

--- app/views/scrapbook-page/scrapbook-page.js
+++ app/views/scrapbook-page/scrapbook-page.js
@@ -1,12 +1,11 @@
 var observable = require("tns-core-modules/data/observable");
 var observableArray = require("tns-core-modules/data/observable-array");
 var frame = require("ui/frame");
+var fileSystemService = require("~/data/fileSystemService")
 
-function scrapbookPageModel() {
+function scrapbookPageModel(id) {
     var model = new observable.Observable();
-        // year = "1988";
-        // month = "10";
-        // day = "19";
+    model.id = id;  
         model.genders = ["Female", "Male", "Other"];
         model.calcAge = function(year, month, day) {
             var date = new Date(year, month, day);
@@ -21,7 +20,23 @@ function scrapbookPageModel() {
 
 exports.onLoaded = function(args) {
     var page = args.object;
-    var scrapbook;
+    var scrapbook = new observable.fromObject({
+        pages: new observableArray.ObservableArray()
+    });
+    var pages = fileSystemService.fileSystemService.getPages();
+    if(pages.length !== 0) {
+        pages.array.forEach(item => {
+        var model = new scrapbookPageModel(item.id);
+
+        model.title = item.title;
+        model.gender = item.gender;
+        model.year = item.year;
+        model.month = item.month;
+        model.day = item.day;
+
+        scrapbook.pages.push(model);
+    });
+    }
     page.year = "1988";
     page.month = "10";
     page.day = "19";
@@ -38,6 +53,16 @@ exports.onLoaded = function(args) {
     page.bindingContext = scrapbook;
 };
 
+exports.onAddTap = function(args) { 
+    var page = args.object;
+    var scrapbook = page.bindingContext;
+    scrapbook.pages.push(new scrapbookPageModel()); 
+    
+    frame.topmost().navigate({
+        moduleName: "views/scrapbookUpdate-page/scrapbookUpdate-page",
+        context: { model: scrapbook, index: scrapbook.pages.length - 1 } 
+    });
+};
 exports.onItemTap = function(args) {
     var page = args.object;
     var scrapbook = page.bindingContext;
